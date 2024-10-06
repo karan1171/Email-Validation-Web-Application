935823e20b8eb05b3d2a148d870a6d3a0a5bad1c
karan1171 <karanm98033@gmail.com>
2024-10-06 12:33:55 +0530

    Intial commit

---
+++ js/index.js
@@ -0,0 +1,26 @@
+console.log("This is my script")
+
+
+
+
+
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    let key = "ema_live_ld04YkBDwUJYJuTfhlxtqEtMN6fBpG13RnguOpAV"
+    let email = document.getElementById("username").value 
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if(result[key] !== "" && result[key]!== " "){ 
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+
+    console.log(str)
+    resultCont.innerHTML = str
+})
+
